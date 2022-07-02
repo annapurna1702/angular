886cbe9fc87c5f212f29396381f7b33f27b22f00
annapurna1702 <annapurnapadmaprema@gmail.com>
2022-07-02 13:47:18 +0530

    Ongoing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TaskComponent } from './task/task.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+    TaskComponent
   ],
   imports: [
     BrowserModule,
